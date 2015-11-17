1326b5d544c4c073c99eb2ee8c048c488800a3c2
Raquel <rlawrenc@ggc.edu>
2015-11-16 23:06:45 -0500

    main activities and classes added

---
+++ app/src/main/java/org/ici/education/ici/Events.java
@@ -0,0 +1,20 @@
+package org.ici.education.ici;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Events extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
